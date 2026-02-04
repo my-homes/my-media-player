97c31a6bc6afbed8977013e399d9870d289f62ca
JavaCommons Technologies <javacommons@gmail.com>
2026-02-04 17:32:41 +0900

    .

--- Form1.cs
+++ Form1.cs
@@ -30,10 +30,6 @@ namespace MyMediaPlayer
             axWindowsMediaPlayer1.Ctlenabled = true;            // ダブルクリックによるフルスクリーン出力を無効化
             axWindowsMediaPlayer1.enableContextMenu = true;     // 右クリックによるコンテキストメニューの出力を無効化
         }
-        //private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
-        //{
-
-        //}
         private void button1_Click(object sender, EventArgs e)
         {
             axWindowsMediaPlayer1.URL = @"C:\Users\user\Music\@1080p\[1080p]  Balo TikTok 【抖音背包】 『Everytime We Touch (Original Mix) - xxxCr3 ｜ 2022抖音最火的歌曲 ｜ Trending TikTok』 【ID：TQ_oIxIDKTA】.mp4";
@@ -48,7 +44,6 @@ namespace MyMediaPlayer
             {
                 // 1. Create a new, empty playlist in the library
                 IWMPPlaylist myPlayList = axWindowsMediaPlayer1.playlistCollection.newPlaylist("MyPlayList");
-
                 // 2. Iterate through the selected files
                 foreach (string file in openFileDialog.FileNames)
                 {
@@ -58,10 +53,8 @@ namespace MyMediaPlayer
                     // Add the media item to the playlist
                     myPlayList.appendItem(mediaItem);
                 }
-
                 // 3. Set the newly created playlist as the current playlist to start playback
                 axWindowsMediaPlayer1.currentPlaylist = myPlayList;
-
                 // Optional: Start playback (it might start automatically depending on control settings)
                 axWindowsMediaPlayer1.Ctlcontrols.play();
             }
