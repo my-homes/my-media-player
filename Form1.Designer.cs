821d931b2ab95546edf4ca6549eae285dd24502d
JavaCommons Technologies <javacommons@gmail.com>
2026-02-04 23:08:03 +0900

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,15 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(724, 96);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // axWindowsMediaPlayer1
             // 
@@ -53,39 +47,78 @@
             this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
             this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(595, 293);
             this.axWindowsMediaPlayer1.TabIndex = 0;
-            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
             // 
-            // button2
+            // toolStrip1
             // 
-            this.button2.Location = new System.Drawing.Point(713, 200);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripLabel1,
+            this.toolStripLabel2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(98, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(98, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel2.Text = "toolStripLabel2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,55 +1,184 @@
-﻿using System;
+﻿using AxWMPLib;
+using Global;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using AxWMPLib;
-using Global;
+using WMPLib;
 using static Global.EasyObject;
 
 namespace MyMediaPlayer
 {
     public partial class Form1 : Form
     {
-        WMPLib.WindowsMediaPlayer mediaPlayer = new WMPLib.WindowsMediaPlayer();
-
+        AxWMPLib.AxWindowsMediaPlayer MediaPlayer;
+        System.Windows.Forms.Timer Timer = new System.Windows.Forms.Timer();
         public Form1()
         {
             InitializeComponent();
-            ReallocConsole();
+            MediaPlayer = axWindowsMediaPlayer1;
+            this.Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            this.StartPosition = FormStartPosition.Manual;
+            Rectangle screen = Screen.FromPoint(Cursor.Position).WorkingArea;
+            this.ClientSize = new Size((int)(screen.Width * 0.75), (int)(screen.Height * 0.75)); /**/
+            //this.Size = new Size((int)(screen.Width * 0.75), (int)(screen.Height * 0.75)); /**/
+            int w = Width >= screen.Width ? screen.Width : (screen.Width + Width) / 2;
+            int h = Height >= screen.Height ? screen.Height : (screen.Height + Height) / 2;
+            this.Location = new Point(screen.Left + (screen.Width - w) / 2, screen.Top + (screen.Height - h) / 2);
+            this.Size = new Size(w, h);
+            //AllocConsole();
             Log("ハロー©");
-            // axWindowsMediaPlayer1.uiMode = "none"; /* 外観のユーザインタフェースを消す */
-            axWindowsMediaPlayer1.settings.autoStart = true; /* 自動開始をオンにする */
-            //this.mediaPlayer.settings.autoStart = true; /* 自動開始をオンにする */
+            //this.AllowDrop = true;
+            Timer.Interval = 1000;
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+            //panel1.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom;
         }
-
-        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            axWindowsMediaPlayer1.URL = @"C:\Users\user\Music\@1080p\[1080p]  Balo TikTok 【抖音背包】 『Everytime We Touch (Original Mix) - xxxCr3 ｜ 2022抖音最火的歌曲 ｜ Trending TikTok』 【ID：TQ_oIxIDKTA】.mp4";
-            //axWindowsMediaPlayer1.Ctlcontrols.play();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            //var pl = axWindowsMediaPlayer1.playlistCollection.newPlaylist(@"C:\Users\user\Music\mine.m3u");
-            //Echo(pl.count);
-            this.mediaPlayer.URL = @"C:\Users\user\Music\@1080p\[1080p]  Balo TikTok 【抖音背包】 『Everytime We Touch (Original Mix) - xxxCr3 ｜ 2022抖音最火的歌曲 ｜ Trending TikTok』 【ID：TQ_oIxIDKTA】.mp4";
-            this.mediaPlayer.controls.play();
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            axWindowsMediaPlayer1.Dock = DockStyle.Fill;
+            //panel1.Dock = DockStyle.Fill;
+            //panel1.Controls.Add(this.MediaPlayer);
+            MediaPlayer.Dock = DockStyle.Fill;
+            SetWMPVolume(100);
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var curMedia = MediaPlayer.currentMedia;
+            if (curMedia != null)
+            {
+                string sourceURL = curMedia.sourceURL;
+                //Log(sourceURL, "sourceURL");
+                string fileName = Path.GetFileName(sourceURL);
+                //Log(fileName, "fileName");
+                if (this.Text != fileName)
+                {
+                    this.Text = fileName;
+                }
+                int duration = (int)MediaPlayer.currentMedia.duration;
+                int curPosition = (int)MediaPlayer.Ctlcontrols.currentPosition;
+                TimeSpan span1 = new TimeSpan(0, 0, duration);
+                TimeSpan span2 = new TimeSpan(0, 0, curPosition);
+                toolStripLabel1.Text = String.Format("{0} / {1}  ", span2.ToString(), span1.ToString());
+                toolStripLabel2.Text = sourceURL;
+            }
+            else
+            {
+                toolStripLabel1.Text = "";
+                toolStripLabel2.Text = "";
+            }
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if ((keyData & Keys.KeyCode) == Keys.Space)
+            {
+                if (MediaPlayer.playState == WMPLib.WMPPlayState.wmppsPaused || MediaPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+                    MediaPlayer.Ctlcontrols.play();
+                else
+                    MediaPlayer.Ctlcontrols.pause();
+                return true;
+            }
+            if ((keyData & Keys.KeyCode) == Keys.C)
+                CopyMediaPlayerInfo();
+            if ((keyData & Keys.KeyCode) == Keys.Left)
+                OnKeyDownLeft();
+            if ((keyData & Keys.KeyCode) == Keys.Right)
+                OnKeyDownRight();
+
+            return base.ProcessDialogKey(keyData);
+        }
+        void CopyMediaPlayerInfo()
+        {
+            var curMedia = MediaPlayer.currentMedia;
+            if (curMedia != null)
+            {
+                string sourceURL = curMedia.sourceURL;
+                int duration = (int)MediaPlayer.currentMedia.duration;
+                int curPosition = (int)MediaPlayer.Ctlcontrols.currentPosition;
+                TimeSpan span1 = new TimeSpan(0, 0, duration);
+                TimeSpan span2 = new TimeSpan(0, 0, curPosition);
+                string str = String.Format(
+                    "ファイルパス：{0}\n長さ：{1}\n現在位置：{2}",
+                    sourceURL, span1.ToString(), span2.ToString());
+                Clipboard.SetText(str);
+            }
+        }
+        void OnKeyDownLeft()
+        {
+            double value = MediaPlayer.Ctlcontrols.currentPosition;
+            if (Control.ModifierKeys != Keys.Control)
+                value--;
+            else
+                value -= 10;
+
+            if (value > 0)
+            {
+                MediaPlayer.Ctlcontrols.currentPosition = value;
+            }
+            if (MediaPlayer.playState == WMPLib.WMPPlayState.wmppsPaused || MediaPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+                MediaPlayer.Ctlcontrols.play();
+        }
+        void OnKeyDownRight()
+        {
+            double value = MediaPlayer.Ctlcontrols.currentPosition;
+            if (Control.ModifierKeys != Keys.Control)
+                value++;
+            else
+                value += 10;
+            if (value < MediaPlayer.currentMedia.duration)
+            {
+                MediaPlayer.Ctlcontrols.currentPosition = value;
+            }
+            if (MediaPlayer.playState == WMPLib.WMPPlayState.wmppsPaused || MediaPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+                MediaPlayer.Ctlcontrols.play();
+        }
+        private void SetWMPVolume(int volume)
+        {
+            // Ensure volume is within the valid range (0 to 100)
+            if (volume >= 0 && volume <= 100)
+            {
+                MediaPlayer.settings.volume = volume;
+            }
+            else
+            {
+                // Handle invalid input if necessary
+                Console.WriteLine("Volume must be between 0 and 100.");
+            }
+        }
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            MediaPlayer.URL = @"C:\Users\user\Music\@1080p\[1080p]  Balo TikTok 【抖音背包】 『Everytime We Touch (Original Mix) - xxxCr3 ｜ 2022抖音最火的歌曲 ｜ Trending TikTok』 【ID：TQ_oIxIDKTA】.mp4";
+            //axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media Files|*.mp3;*.m4a;*.wma;*.wav;*.mp4;*.wmv|All Files|*.*";
+            openFileDialog.Multiselect = true; // Allow multiple selections
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // 1. Create a new, empty playlist in the library
+                IWMPPlaylist myPlayList = MediaPlayer.playlistCollection.newPlaylist("MyPlayList");
+                // 2. Iterate through the selected files
+                foreach (string file in openFileDialog.FileNames)
+                {
+                    // Create a new media item from the file path
+                    IWMPMedia mediaItem = MediaPlayer.newMedia(file);
+
+                    // Add the media item to the playlist
+                    myPlayList.appendItem(mediaItem);
+                }
+                // 3. Set the newly created playlist as the current playlist to start playback
+                MediaPlayer.currentPlaylist = myPlayList;
+                // Optional: Start playback (it might start automatically depending on control settings)
+                MediaPlayer.Ctlcontrols.play();
+            }
         }
     }
 }
