a625b362094fb335441ac73d1e8e7c437364ddb1
JavaCommons Technologies <javacommons@gmail.com>
2026-02-04 23:20:27 +0900

    .

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 set -uvx
 set -e
 cwd=`pwd`
-rm -rf obj bin
+rm -rf obj bin *.exe
 dotnet restore
+
 msbuild.exe -restore -verbosity:quiet
 msbuild.exe -t:Rebuild -p:Configuration=Release -verbosity:quiet
 find ./bin/Release -name *.exe
-cd $cwd/bin/Release/net462
-ilmerge.exe -out:$cwd/mmp.exe -wildcards mmp.exe *.dll
+
+#cd $cwd/bin/Release/net462
+#ilmerge.exe -out:$cwd/mmp.exe -wildcards mmp.exe *.dll
+cd $cwd/bin/Release
+ilmerge.exe -out:$cwd/MyMediaPlayer.exe -wildcards MyMediaPlayer.exe *.dll
+
 cd $cwd
-ls -lh mmp.*
+ls -lh *.exe *.pdb
